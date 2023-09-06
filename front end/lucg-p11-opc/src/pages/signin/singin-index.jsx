05689f5a46a16c241aba949202567d0c56c7be47
Luc Gouzy <lg-1@live.fr>
2023-09-06 17:57:06 +0200

    state now works correctly for login

--- front end/lucg-p11-opc/src/pages/signin/singin-index.jsx
+++ front end/lucg-p11-opc/src/pages/signin/singin-index.jsx
@@ -3,93 +3,90 @@ import Navbar from "../../components/navbar";
 import Footer from "../../components/footer";
 import { login } from "../../reducer/authSlice";
 import store from "../../store/store";
-import { Navigate } from "react-router";
 import { useNavigate } from "react-router";
 
 function Signin() {
-  const Navigate = useNavigate();
   const form = document.getElementById("login-form");
-  if (form) {
-    const mailInput = document.getElementById("email");
-    const passwordInput = document.getElementById("password");
-    const form = document.getElementById("login-form");
+  const Navigate = useNavigate();
 
-    function ResolveUsername(token) {
-      const apiUrl = "http://localhost:3001/api/v1/user/profile";
-      const requestOptions = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: null,
-      };
+  const mailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
 
-      fetch(apiUrl, requestOptions)
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error("Network response was not ok");
-          }
-        })
-        .then((data) => {
-          console.log("API Response:", data);
-          const {
-            body: { firstName, lastName, userName, id },
-          } = data;
+  function ResolveUsername(token) {
+    const apiUrl = "http://localhost:3001/api/v1/user/profile";
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: null,
+    };
 
-          store.dispatch(
-            login({
-              isAuthenticated: true,
-              firstName,
-              lastName,
-              userName,
-              id,
-            }),
-            Navigate("/")
-          );
-        })
-        .catch((error) => {
-          console.error("API Error:", error);
-        });
-    }
-    form.addEventListener("submit", async (e) => {
-      e.preventDefault();
-
-      const mail = mailInput.value;
-      const password = passwordInput.value;
-
-      const apiUrl = "http://localhost:3001/api/v1/user/login";
-      const requestOptions = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: mail,
-          password: password,
-        }),
-      };
-
-      try {
-        const response = await fetch(apiUrl, requestOptions);
+    fetch(apiUrl, requestOptions)
+      .then((response) => {
         if (response.ok) {
-          const data = await response.json();
-          const {
-            body: { token },
-          } = data;
-          ResolveUsername(token);
-
-          console.log("Login successful:", data);
+          return response.json();
         } else {
-          console.error("Login failed:", response.statusText);
+          throw new Error("Network response was not ok");
         }
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    });
+      })
+      .then((data) => {
+        console.log("API Response:", data);
+        const {
+          body: { firstName, lastName, userName, id },
+        } = data;
+
+        store.dispatch(
+          login({
+            isAuthenticated: true,
+            firstName,
+            lastName,
+            userName,
+            id,
+          })
+        );
+        Navigate("/user");
+      })
+      .catch((error) => {
+        console.error("API Error:", error);
+      });
   }
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const mail = mailInput.value;
+    const password = passwordInput.value;
+
+    const apiUrl = "http://localhost:3001/api/v1/user/login";
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: mail,
+        password: password,
+      }),
+    };
+
+    try {
+      const response = await fetch(apiUrl, requestOptions);
+      if (response.ok) {
+        const data = await response.json();
+        const {
+          body: { token },
+        } = data;
+        ResolveUsername(token);
+
+        console.log("Login successful:", data);
+      } else {
+        console.error("Login failed:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  });
 
   return (
     <div className="App">
